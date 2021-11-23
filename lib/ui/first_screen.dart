11575f4008e391b6104e40d997eae5ca2338d084
harunalbayrak <harunnalbayrakk278@gmail.com>
2021-11-23 13:41:21 +0000

    First commit

---
+++ lib/ui/first_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to the second screen when tapped.
+          },
+          child: const Text('Launch screen'),
+        ),
+      ),
+    );
+  }
+}
