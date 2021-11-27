b335abefcaaeae54795eed0acc0e1a6882d9bb57
harunalbayrak <harunnalbayrakk278@gmail.com>
2021-11-27 13:00:37 +0000

    Extract some widgets

--- lib/ui/blocked_activities/blocked_activities_2.dart
+++ lib/ui/blocked_activities/blocked_activities_2.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:graduation_app/constants/box_decorations.dart';
 import 'package:graduation_app/widgets/build_background.dart';
+import 'package:graduation_app/widgets/list_tiles.dart';
+import 'package:graduation_app/widgets/padding_bold_text.dart';
 import 'package:graduation_app/constants/text_styles.dart';
-import 'package:graduation_app/constants/colors.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 
 class BlockedActivities2 extends StatefulWidget {
   BlockedActivities2({Key? key}) : super(key: key);
@@ -13,14 +13,6 @@ class BlockedActivities2 extends StatefulWidget {
 }
 
 class _BlockedActivities2State extends State<BlockedActivities2> {
-  final titles = ["List 1", "List 2", "List 3"];
-  final subtitles = [
-    "Here is list 1 subtitle",
-    "Here is list 2 subtitle",
-    "Here is list 3 subtitle"
-  ];
-  final double paddingListTile = 12.0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,68 +34,14 @@ class _BlockedActivities2State extends State<BlockedActivities2> {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          buildAListTile1("www.telegram.org"),
-          paddingBoldText("Actions"),
-          buildAListTile2("Add to Allowed", Icons.done),
-          buildAListTile2("Copy to Clipboard", Icons.copy_all),
-          paddingBoldText("Information"),
-          buildAListTile3("Application", "Telegram"),
-          buildAListTile3("Asd asdas asdad", "Lorem ipsum"),
+          buildListTile1("www.telegram.org"),
+          paddingBoldText(context, "Actions"),
+          buildListTile2("Add to Allowed", Icons.done),
+          buildListTile2("Copy to Clipboard", Icons.copy_all),
+          paddingBoldText(context, "Information"),
+          buildListTile3("Application", "Telegram"),
+          buildListTile3("Asd asdas asdad", "Lorem ipsum"),
         ],
-      ),
-    );
-  }
-
-  Widget buildAListTile1(String str) {
-    return ListTile(
-      contentPadding: EdgeInsets.all(paddingListTile),
-      leading: const CircleAvatar(
-        backgroundImage: NetworkImage(
-            "https://images.unsplash.com/photo-1547721064-da6cfb341d50"),
-      ),
-      title: AutoSizeText(
-        str,
-        style: textStyle2,
-      ),
-    );
-  }
-
-  Widget paddingBoldText(String str) {
-    return Padding(
-      padding: EdgeInsets.all(paddingListTile),
-      child: Text(
-        str,
-        style: textStyle4(context),
-      ),
-    );
-  }
-
-  Widget buildAListTile2(String str, IconData iconData) {
-    return ListTile(
-      contentPadding: EdgeInsets.fromLTRB(
-          paddingListTile, 0, paddingListTile, paddingListTile),
-      trailing: Icon(
-        iconData,
-        color: gray,
-      ),
-      title: AutoSizeText(
-        str,
-        style: textStyle2,
-      ),
-    );
-  }
-
-  Widget buildAListTile3(String str1, String str2) {
-    return ListTile(
-      contentPadding: EdgeInsets.fromLTRB(
-          paddingListTile, 0, paddingListTile, paddingListTile),
-      title: AutoSizeText(
-        str1,
-        style: textStyle2,
-      ),
-      subtitle: AutoSizeText(
-        str2,
-        style: textStyle2,
       ),
     );
   }
