e90cfc59b53ee573bfb71653a8fa2cf5f4f9c2ba
harunalbayrak <harunnalbayrakk278@gmail.com>
2021-11-28 00:07:12 +0000

    Add vertical bar charts

--- lib/ui/statistics/statistics.dart
+++ lib/ui/statistics/statistics.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:graduation_app/constants/colors.dart';
+import 'package:graduation_app/constants/text_styles.dart';
+import 'package:graduation_app/constants/env.dart';
 import 'package:graduation_app/widgets/build_background.dart';
+import 'package:graduation_app/widgets/vertical_bar_chart_2.dart';
+import 'package:graduation_app/widgets/vertical_bar_chart.dart';
+import 'package:graduation_app/models/chart_data.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Statistics extends StatefulWidget {
   const Statistics({Key? key}) : super(key: key);
@@ -9,20 +17,101 @@ class Statistics extends StatefulWidget {
 }
 
 class _StatisticsState extends State<Statistics> {
+  final double width = 7;
+  late List<BarChartGroupData> showingBarGroups;
+  late List<BarChartGroupData> showingBarGroups2;
+
+  @override
+  void initState() {
+    super.initState();
+    final barGroup1 = makeGroupData(0, 5, 12);
+    final barGroup2 = makeGroupData(1, 16, 12);
+    final barGroup3 = makeGroupData(2, 18, 5);
+    final barGroup4 = makeGroupData(3, 20, 16);
+    final barGroup5 = makeGroupData(4, 17, 6);
+    final barGroup6 = makeGroupData(5, 19, 1.5);
+    final barGroup7 = makeGroupData(6, 10, 1.5);
+
+    final barGroup8 = makeGroupData2(0, 17);
+    final barGroup9 = makeGroupData2(1, 14);
+    final barGroup10 = makeGroupData2(2, 13);
+    final barGroup11 = makeGroupData2(3, 12);
+    final barGroup12 = makeGroupData2(4, 10);
+    final barGroup13 = makeGroupData2(5, 9);
+    final barGroup14 = makeGroupData2(6, 7);
+
+    final items = [
+      barGroup1,
+      barGroup2,
+      barGroup3,
+      barGroup4,
+      barGroup5,
+      barGroup6,
+      barGroup7,
+    ];
+
+    final items2 = [
+      barGroup8,
+      barGroup9,
+      barGroup10,
+      barGroup11,
+      barGroup12,
+      barGroup13,
+      barGroup14,
+    ];
+
+    showingBarGroups = items;
+    showingBarGroups2 = items2;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Statistics')),
+      appBar: AppBar(title: const Text('Filters')),
       body: Stack(
         children: [
           buildBackground(),
-          buildContainer(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(paddingOverall),
+                child: Text(
+                  "Total/Blocked Activities per day",
+                  style: textStyle5(context, 20),
+                ),
+              ),
+              buildVerticalBarChart(showingBarGroups),
+              buildVerticalBarChart2(showingBarGroups2),
+            ],
+          ),
         ],
       ),
     );
   }
 
-  Widget buildContainer() {
-    return Container();
+  BarChartGroupData makeGroupData(int x, double y1, double y2) {
+    return BarChartGroupData(barsSpace: 4, x: x, barRods: [
+      BarChartRodData(
+        y: y1,
+        colors: [lightBlue, orange],
+        width: width,
+      ),
+      BarChartRodData(
+        y: y2,
+        colors: [lightBlue, blue],
+        width: width,
+      ),
+    ]);
+  }
+
+  BarChartGroupData makeGroupData2(int x, double y1) {
+    return BarChartGroupData(barsSpace: 4, x: x, barRods: [
+      BarChartRodData(
+        y: y1,
+        colors: [lightBlue, orange],
+        width: width,
+      ),
+    ]);
   }
 }
---
+++ lib/widgets/vertical_bar_chart.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:graduation_app/constants/colors.dart';
+
+Widget buildVerticalBarChart(List<BarChartGroupData> showingBarGroups) {
+  return AspectRatio(
+    aspectRatio: 1.5,
+    child: Card(
+      elevation: 0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      color: darkBlue,
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: BarChart(
+                BarChartData(
+                  maxY: 20,
+                  barTouchData: BarTouchData(
+                    touchTooltipData: BarTouchTooltipData(
+                      tooltipBgColor: Colors.grey,
+                      getTooltipItem: (_a, _b, _c, _d) => null,
+                    ),
+                  ),
+                  titlesData: FlTitlesData(
+                    show: true,
+                    rightTitles: SideTitles(showTitles: false),
+                    topTitles: SideTitles(showTitles: false),
+                    bottomTitles: SideTitles(
+                      showTitles: true,
+                      getTextStyles: (context, value) => const TextStyle(
+                          color: Color(0xff7589a2),
+                          fontWeight: FontWeight.w700,
+                          fontSize: 14),
+                      margin: 20,
+                      getTitles: (double value) {
+                        switch (value.toInt()) {
+                          case 0:
+                            return 'Mn';
+                          case 1:
+                            return 'Te';
+                          case 2:
+                            return 'Wd';
+                          case 3:
+                            return 'Tu';
+                          case 4:
+                            return 'Fr';
+                          case 5:
+                            return 'St';
+                          case 6:
+                            return 'Sn';
+                          default:
+                            return '';
+                        }
+                      },
+                    ),
+                    leftTitles: SideTitles(
+                      showTitles: true,
+                      getTextStyles: (context, value) => const TextStyle(
+                          color: Color(0xff7589a2),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14),
+                      margin: 8,
+                      reservedSize: 28,
+                      interval: 1,
+                      getTitles: (value) {
+                        if (value == 0) {
+                          return '1K';
+                        } else if (value == 10) {
+                          return '5K';
+                        } else if (value == 19) {
+                          return '10K';
+                        } else {
+                          return '';
+                        }
+                      },
+                    ),
+                  ),
+                  borderData: FlBorderData(
+                    show: false,
+                  ),
+                  barGroups: showingBarGroups,
+                  gridData: FlGridData(show: true),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
